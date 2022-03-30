3fc747334a4d3c538bb338e30993149169f7b319
yeibudu <yeimerburitica1997@gmail.com>
2022-03-30 06:36:26 -0500

    improve countries CRUD

---
+++ TiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Web.Models;
+
+namespace TiendaOnline.Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ): base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
